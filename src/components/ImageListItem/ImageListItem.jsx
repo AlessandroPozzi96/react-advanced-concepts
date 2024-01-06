f62374cdf96e842298f37409103a390d76045260
AlessandroPozzi96 <alessandro.pozzi72@gmail.com>
2024-01-06 13:05:50 +0100

    Add custom hooks + Stylising imageListItem

--- src/components/ImageListItem/ImageListItem.jsx
+++ src/components/ImageListItem/ImageListItem.jsx
@@ -1,5 +1,21 @@
-import s from "./style.module.css";
+import "./imageListItem.css";
 
 export function ImageListItem({ img }) {
-  return <img src={img.download_url} className={s.img} />;
+  return (
+    <div className="card">
+      <a href={img.url}>
+        <img src={img.download_url} className="img" alt="test" />
+      </a>
+      <div className="card_banner">
+        <img src={img.download_url} className="card_thumb" alt="test" />
+        <div className="card_text">
+          <h3 className="card_title">{img.author}</h3>
+          <div className="card_status">
+            Original size: {img.height}x{img.width}
+          </div>
+          <button>Download</button>
+        </div>
+      </div>
+    </div>
+  );
 }
